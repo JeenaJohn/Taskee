c33594066c0b19602bac003107830016202ff144
Jeena John <jeena.john@gmail.com>
2020-09-06 22:20:39 -0700

    Added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,79 @@
 import React, { useState, useEffect } from "react";
+import firebase, { auth, provider } from "./firebase.js";
+
+import './App.css';
+
+import logo from "./resources/images/calendar-logo.svg";
+
 
 function App(){
+
+  const [user, setUser] = useState(null);
+  const [userID, setUserID] = useState(null);
+
+  const logout = () => {
+    auth.signOut().then(() => {
+      setUser(null);
+      setUserID(null);
+    });
+  };
+
+  const login = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      const user = result.user;
+      setUser(user);
+      setUserID(user.uid);
+    });
+  };
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        setUserID(user.uid);
+      }
+    });
+  });
+
  return(
-   <div></div>
+   <div>
+     <header>
+        <nav className="nav-bar">
+          <div className="row">
+            <div className="header__logo-box">
+              <img src={logo} alt="Logo" className="header__logo" />
+            </div>
+            <div className="header__userinfo">
+              {user ? (
+                <h4>
+                  Welcome {user.displayName}
+                  <button className="btn btn-medium-ghost" onClick={logout}>
+                    Logout
+                  </button>
+                </h4>
+              ) : (
+                <button className="btn btn-medium btn-nav-bar" onClick={login}>
+                  Login with Google
+                </button>
+              )}
+            </div>
+          </div>
+        </nav>
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">Taskee</span>
+            <span className="heading-primary--sub">
+              Reminder for your personal tasks
+            </span>
+          </h1>
+
+          <a href="#section-list-data" className="btn btn-header">
+            Show my Tasks
+          </a>
+        </div>
+      </header>
+
+   </div>
  );
 }
 
