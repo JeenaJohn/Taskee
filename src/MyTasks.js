ee2ace94b0a086cb0ac3a43a162dce6bb66f0742
Jeena John <jeena.john@gmail.com>
2020-09-07 11:33:46 -0700

    Added user msg to login if userID is null

--- src/MyTasks.js
+++ src/MyTasks.js
@@ -2,12 +2,32 @@ import React from "react";
 import AddCard from "./AddCard";
 import Buckets from "./Buckets";
 
+function MyTasks(props) {
+  return (
+    <div>
+      <section id="section-list-data" className="section-list-data">
+        <div className="u-center-text u-margin-bottom-medium">
+          <h2 className="heading-secondary bg-color-blue">My Tasks</h2>
+        </div>
+        {props.userID === null ? (
+          <p className="paragraph u-text-left u-text-color-red">
+            You have to login to use this app.
+          </p>
+        ) : null}
 
-function MyTasks(){
-    return(<div>
+        <div className="box-task">
+          <h3
+            className="heading-tertiary task-headings
+         u-text-left u-margin-bottom-small"
+          >
+            Add new Task
+          </h3>
 
-    </div>);
-
+          <AddCard />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default MyTasks;
